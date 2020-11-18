4680fd9d16fd874f4d6bbe275f4b6f1ecd8fec22
na-vural <vuraln18@itu.edu.tr>
2020-11-18 23:53:25 +0300

    File name is changed, comments are added, main arguments are implemented.

--- yprime.c
+++ yprime.c
@@ -9,16 +9,15 @@ struct thread_data
 {
         mpz_t *num;
         mpz_t *limit;
-        signed char *check_byt;
         unsigned char thread_id;
         unsigned char *num_of_threads;
-        unsigned short int inc_seq_index;
+        unsigned short int inc_seq_len;
         unsigned char *inc_seq;
 };
 
 
 void create_eliminated_seq( unsigned char inc_seq[],
-                            unsigned short int inc_seq_index,
+                            unsigned short int inc_seq_len,
                             unsigned char primes_array[],
                             unsigned char primes_index)
 {
@@ -44,41 +43,50 @@ void create_eliminated_seq( unsigned char inc_seq[],
          * until find the all modulus of the our elimination primes.*/
         unsigned short int step = 1; /* First step for addition */
         
-        unsigned char addend = 2; /* The value of the addition in an each step. 
+        unsigned char inc_value = 2; /* The value of the addition in an each step. 
                                    * It is 2 because we just count odd numbers.*/
         
         for (i = 0 ; i < primes_index ; ) {
-                if ( (1 + addend) % primes_array[i] == 0 ) {
+                if ( (1 + inc_value) % primes_array[i] == 0 ) {
                         mod_result[i] = step;
                         step++;
-                        addend += 2;
+                        inc_value += 2;
                         i++;
                 } else {
                         step++;
-                        addend += 2;
+                        inc_value += 2;
                 }
         }
 
-        /* Finding the inc value to jump over the numbers
+        /* To find the inc value to jump over the numbers
          * that divisible with our elimination primes.
          * The mod list created above is used in this algorithm.
-         * Simply, we check the n'th step and */
+         * Simply, we check the n'th step and decide whether
+         * this step is divisible with our primes or not.
+         * If a step is divisible, then we add 2 to inc value,
+         * and look at next step. If the next step also divisible
+         * we again add 2 to inc value. Until find a step that are
+         * not divisible with our primes, we continue to this proccess.
+         * When find a such step that non-divisible, we just store
+         * the current inc value, and initialize this inc value for
+         * the proccess continuation. To simplify this, we created 
+         * and use mod_result array. */
         step = 1;
-        for (i = 0 ; i < inc_seq_index ; i++) {
-                addend = 2;
+        for (i = 0 ; i < inc_seq_len ; i++) {
+                inc_value = 2;
                 check = 0;
                 while (check < primes_index) {
                         for (j = 0 ; j < primes_index ; j++) {
                             if (step % primes_array[j] == mod_result[j]) {
                                     step++;
-                                    addend += 2;
+                                    inc_value += 2;
                                     check = 0;
                             } else {
                                     check++;
                             }
                         }
                 }
-                inc_seq[i] = addend;
+                inc_seq[i] = inc_value;
                 step++;
         }
 }
@@ -89,39 +97,50 @@ void *check( void *threadargs )
         struct thread_data *args = (struct thread_data*) threadargs;
         mpz_t *num = args->num;
         mpz_t *limit = args->limit;
-        signed char *check_byt = args->check_byt;
         unsigned char thread_id = args->thread_id;
         unsigned char *num_of_threads = args->num_of_threads;
         unsigned char *inc_seq = args->inc_seq;
-        unsigned short int inc_seq_index = args->inc_seq_index;
+        unsigned short int inc_seq_len = args->inc_seq_len;
  
         mpz_t divisor;
         mpz_init(divisor);
         mpz_set_ui(divisor, 1);
 
+        /* For example if there are 4 threads,
+         * zeroth, first, second, third,
+         * we just convert zeroth thread to fourth
+         * thread. */
         if ( thread_id == 0 ) {
                 thread_id = *num_of_threads;
         }
 
+        /* We create special steps for all threads.
+         * So each of them start the division with
+         * different values. */
         unsigned char step_init = 0;
         for ( unsigned int i = 0 ; i < thread_id ; i++ ) {
                 step_init += inc_seq[i];
         }
-        
         mpz_add_ui(divisor, divisor, step_init);
 
-        unsigned char parallel_inc[inc_seq_index];
-        for ( unsigned int i = 0 ; i < inc_seq_index ; i++ ) {
+        /* We share out the divisor values between threads.
+         * To achieve this, we create different increment steps
+         * for all threads. */
+        unsigned char parallel_inc[inc_seq_len];
+        for ( unsigned int i = 0 ; i < inc_seq_len ; i++ ) {
             parallel_inc[i] = 0;
             for ( unsigned char j = 0 ; j < *num_of_threads ; j++ ) {
                 parallel_inc[i] 
                 +=
-                inc_seq[(i * (*num_of_threads) + thread_id + j) % inc_seq_index];
+                inc_seq[(i * (*num_of_threads) + thread_id + j) % inc_seq_len];
             }
         }
 
+        /* Here is brute force section.
+         * We divide our prime candidate by all the numbers that are odd 
+         * and not divisible with our elimination primes. */
         for ( unsigned char i = 0 ; mpz_cmp(*limit, divisor) >= 0 ; i++ ) {
-                i %= inc_seq_index;
+                i %= inc_seq_len;
                 if ( mpz_divisible_p(*num, divisor) != 0 ) {
                         gmp_printf("Not prime. Divisible with %Zd", divisor);
                         exit(0);
@@ -140,14 +159,11 @@ int main(int argc, char *argv[])
         
         if ( (argc != 2) || (mpz_init_set_str(num, argv[1], 10) != 0) ) {
                 printf("%s", "\nUsage: yprime <number>\n\n");
-                return -1;
+                return 1;
         }
-
-//        mpz_init(num);
-//        printf("%s", "Gimme the integer\n---> ");
-//        gmp_scanf("%Zd", num);
         gmp_printf("Given number is %Zd\n", num);
 
+        /* Eliminate 2 and even numbers. */
         if ( mpz_divisible_ui_p(num, 2) != 0 ) {
                 if ( mpz_cmp_ui(num, 2) == 0 ) {
                         printf("%s", "First and last even prime.");
@@ -158,45 +174,51 @@ int main(int argc, char *argv[])
                 }
         }
 
+        /* Our number are checked with elimination primes,
+         * then if the number is not divisible these primes,
+         * we eliminate all of them from future divisions. */
         unsigned char el_primes[] = {3, 5, 7, 11};
-        unsigned char el_primes_index = 4;
-        for ( unsigned char i = 0; i < 4 ; i++ ) {
+        unsigned char el_primes_len = 4;
+        for ( unsigned char i = 0; i < el_primes_len ; i++ ) {
                 if ( mpz_divisible_ui_p(num, el_primes[i]) != 0 ) {
                         if ( mpz_cmp_ui(num, el_primes[i]) == 0 ) {
                                 printf("%s", "Prime");
                                 return 0;
                         } else {
-                                printf( "Not prime divisible with %d",
+                                printf( "Not prime divisible with %d.",
                                         el_primes[i]);
                                 return 0;
                         }
                 }
         }
 
-        unsigned short int inc_seq_index = 480;
-        unsigned char inc_seq[inc_seq_index];
+        /* An algorithm will be added 
+         * to automatically calculate inc_seq_len. */
+        unsigned short int inc_seq_len = 480;
+        unsigned char inc_seq[inc_seq_len];
         create_eliminated_seq(  inc_seq,
-                                inc_seq_index,
+                                inc_seq_len,
                                 el_primes,
-                                el_primes_index);
-
+                                el_primes_len);
+        
+        /* Limit is the square root of our number,
+         * we divide our number by all the numbers 
+         * until this limit. */
         mpz_t limit;
         mpz_init(limit);
         mpz_sqrt(limit, num);
 
+        /* Created threads number will be number of processors. */
         unsigned char num_of_threads = get_nprocs();
-
         pthread_t threads[num_of_threads];
-        signed char check_byt = 0;
-
         struct thread_data threadargs[num_of_threads];
+        /* Thread creation. */
         for ( unsigned char i = 0 ; i < num_of_threads ; i++ ) {
                 threadargs[i].num = &num;
                 threadargs[i].limit = &limit;
-                threadargs[i].check_byt = &check_byt;
                 threadargs[i].thread_id = i;
                 threadargs[i].num_of_threads = &num_of_threads;
-                threadargs[i].inc_seq_index = inc_seq_index;
+                threadargs[i].inc_seq_len = inc_seq_len;
                 threadargs[i].inc_seq = inc_seq;
                 pthread_create( &threads[i],
                                 NULL,
@@ -204,14 +226,11 @@ int main(int argc, char *argv[])
                                 (void *) &threadargs[i]);
         }
 
-        void *thread_ret;
 
         for ( unsigned char i = 0 ; i < num_of_threads ; i++ ) {
-                pthread_join(threads[i], &thread_ret); 
+                pthread_join(threads[i], NULL); 
         }
 
-        //pthread_join(threads[0], NULL);
-        //check(num, limit, 1, inc_seq, 480);
         printf("%s", "PRIME");
         pthread_exit(NULL);
         return 0;
